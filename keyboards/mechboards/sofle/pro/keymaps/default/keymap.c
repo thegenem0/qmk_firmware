5acfb6b6c42e5f18d386b3963824f90a0afd4768
thegenem0 <nemethgergo02@gmail.com>
2025-05-04 16:27:32 +0100

    fix: updates to matrix keycodes

--- keyboards/mechboards/sofle/pro/keymaps/default/keymap.c
+++ keyboards/mechboards/sofle/pro/keymaps/default/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                            KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
                 KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                            KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
                 KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                           KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
-                KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,     KC_MUTE, KC_MPLY,     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
+                KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,     KC_MUTE, RM_TOGG,     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RCTL,
                            KC_NO, KC_NO, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC, MO(1), MO(2), KC_NO, KC_NO
                 ),
 
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              * |      |      |      |      |      |      |                      |      |      |      |  [   |  ]   |  \   |
              * |------+------+------+------+------+------|                      |------+------+------+------+------+------|
              * | TRNS |      | PSCR |      |      |      |-------,      ,-------|  <-  | Down |  Up  |  ->  |  ;   |  '   |
-             * |------+------+------+------+------+------| BKLT  |      | BRTHE |------+------+------+------+------+------|
+             * |------+------+------+------+------+------| BKLT  |      |       |------+------+------+------+------+------|
              * | TRNS |      |      |      |      |      |-------|______|-------|      |      |  ,   |  .   |  /   | TRNS |
              * `-----------------------------------------|  /       /\       \  |-----------------------------------------'
              * |           \ |      |      | TRNS | TRNS | / TRNS  /  \ TRNS  \ |      |      |      |      | /
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_GRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                          KC_NO, KC_NO, KC_NO, KC_MINS, KC_EQL, KC_NO,
                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_BSLS,
                 KC_TRNS, KC_NO, KC_PSCR, KC_NO, KC_NO, KC_NO,                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_SCLN, KC_QUOT,
-                KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     BL_TOGG, BL_BRTG,   KC_NO, KC_NO, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
+                KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO,   KC_NO, KC_NO, KC_COMM, KC_DOT, KC_SLSH, KC_NO,
                                 KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO
     ),
 
@@ -73,8 +73,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-[0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT)},
-[1] = { ENCODER_CCW_CW(BL_DOWN, BL_UP), ENCODER_CCW_CW(BL_BRTG, BL_BRTG)},
-[2] = { ENCODER_CCW_CW(BL_TOGG, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS)},
+[0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(RM_VALD, RM_VALU)},
+[1] = { ENCODER_CCW_CW(KC_MPRV, KC_MNXT), ENCODER_CCW_CW(RM_PREV, RM_NEXT)},
+[2] = { ENCODER_CCW_CW(RM_HUED, RM_HUEU), ENCODER_CCW_CW(RM_SPDU, RM_SPDD)},
 };
 #endif
